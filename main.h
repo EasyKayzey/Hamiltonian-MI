3495e81462d09c861e9a62bd4644a0eeb69f526f
Erez Israeli Miller <erez.m.israeli@gmail.com>
2022-07-13 23:51:11 +0300

    Move T and NT to header

--- main.h
+++ main.h
@@ -43,7 +43,8 @@ const int N_TO = 1;
 const int N_OBS = DIM * N_TO;
 const int N_H = L;
 const int N_FIELDS = 1;
-
+const double HEADER_TIME = 2000;
+const double HEADER_NTD = 500;
 const double MY_PI = 3.14159265358979323846264338327950288419716939937510582097494459230781;
 
 #define USE_LONG_DOUBLE false
